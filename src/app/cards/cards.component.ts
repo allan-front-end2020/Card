616af69dfa115a90992c439c5c98b69e441e57f6
Allan Tavares <allan.tavares@estudante.ufla.br>
2022-03-11 13:11:15 -0300

    retirado o snackbar

--- src/app/cards/cards.component.ts
+++ src/app/cards/cards.component.ts
@@ -1,6 +1,5 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component,  Input } from '@angular/core';
 import { MatSnackBar } from '@angular/material/snack-bar';
-
 
 
 @Component({
